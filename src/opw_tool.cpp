54c87a7a88bafeb6caaaab1c9659a615d12b0a62
jantolsen <janthomasolsen@gmail.com>
2023-10-12 19:58:36 +0200

    Converted to ros-package (compatible with catkin build)

---
+++ src/opw_tool.cpp
@@ -0,0 +1,21 @@
+// Include Header-files:
+// -------------------------------
+    // Standard
+    #include <memory>
+    
+    // OPW
+    #include "opw_kinematics/opw_io.h"
+    // #include "opw_kinematics/opw_kinematics_impl.h"
+    #include "opw_kinematics/opw_kinematics.h"
+    #include "opw_kinematics/opw_macros.h"
+    #include "opw_kinematics/opw_parameters.h"
+    #include "opw_kinematics/opw_parameters_examples.h"
+    #include "opw_kinematics/opw_utilities.h"
+
+// OPW Tool
+// -------------------------------
+int main()
+{
+    // Dummy code to create library
+    return 0;
+}
